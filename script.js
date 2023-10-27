ca1e29a1eb7add2bb777a4494f95ad24c538fb27
Muneeb0077 <muneebasif4213@gmail.com>
2023-10-27 16:10:40 +0500

    Some modifications like added check box

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 let fromInputData = document.getElementById("input");
 let todoList = document.getElementById("todos");
 let addTaskButton = document.getElementById("TaskButtonAdd");
-let deleteTaskButton = document.createElement("button");     
-function addtasks (){
-            let todo = fromInputData.value;
-            let listTasks = document.createElement("li"); 
-            listTasks.innerText = todo;
-            listTasks.style.width = "50vmax";
-            todoList.appendChild(listTasks);
-            fromInputData.value = "";
-            
-            deleteTaskButton.innerText = "Delete";
-            listTasks.appendChild(deleteTaskButton);
-        
-        deleteTaskButton.addEventListener("click", ()=>{
-            listTasks.remove();
-        })
-        editTask(listTasks);
+let deleteTaskButton = document.createElement("button");
+let checkBox = document.createElement("input");
+checkBox.type = "checkbox";
+function addtasks() {
+    let todo = fromInputData.value;
+    let listTasks = document.createElement("li");
+    listTasks.innerText = todo;
+    listTasks.style.width = "50vmax";
+    todoList.appendChild(listTasks);
+    fromInputData.value = "";
+
+    deleteTaskButton.innerText = "Delete";
+    listTasks.appendChild(deleteTaskButton);
+
+
+
+    listTasks.appendChild(checkBox);
+
+
+    deleteTaskButton.addEventListener("click", () => {
+        listTasks.remove();
+    })
+    editTask(listTasks);
+    checkBox.addEventListener("click", () => {
+        if (checkBox.checked) {
+            listTasks.style.textDecoration = "line-through";
+        } else {
+            listTasks.style.textDecoration = "none";
+        }
+    });
+
 }
 
-function editTask(listTasks){
+function editTask(listTasks) {
     let editTaskButton = document.createElement("button");
     editTaskButton.innerText = "Edit";
     listTasks.appendChild(editTaskButton);
-    editTaskButton.addEventListener("click", ()=>{
+    editTaskButton.addEventListener("click", () => {
         let input = document.createElement("input");
         listTasks.appendChild(input);
-        input.addEventListener("dblclick", ()=>{
-            listTasks.innerText= input.value;
+        input.addEventListener("dblclick", () => {
+            listTasks.innerText = input.value;
             listTasks.appendChild(editTaskButton);
             listTasks.appendChild(deleteTaskButton);
+            listTasks.appendChild(checkBox);
         })
-        
+
     })
 }
 
